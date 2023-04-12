ad9a129f8d3502939a19362a5eaffb94d33233eb
Golfantara <golfantara7@gmail.com>
2023-04-12 10:37:44 +0700

    navbar-fixed

---
+++ dist/assets/Fact-ae752a77.js
@@ -0,0 +1,1 @@
+import{a as s}from"./index-89d1aeca.js";const a="/assets/Group74-b0f86397.jpg",t=()=>s("div",{className:"grid place-content-center",children:s("img",{src:a,className:"rounded-xl my-10",alt:""})});export{t as default};
