6e8a4fa647e214a88794aa48a74dea273a5aa0d4
Golfantara <golfantara7@gmail.com>
2023-04-27 14:09:54 +0700

    fixed-navbar-responsive

---
+++ dist/assets/HeadSection-a4d69241.js
@@ -0,0 +1,1 @@
+import{j as t,a as e}from"./index-fb6540b0.js";const x=()=>t("div",{id:"home",children:[e("h1",{className:"text-center text-3xl md:text-7xl px-2 lg:px-32 text-[#C62937] font-bold",children:"FAKTA MENGERIKAN KANKER"}),e("p",{className:"text-center text-lg md:text-xl px-2",children:"Kanker adalah penyebab kematian ketiga terbanyak di indonesia"})]});export{x as default};
--- dist/assets/HeadSection-af156a89.js
+++
@@ -1,1 +0,0 @@
-import{j as t,a as e}from"./index-89d1aeca.js";const x=()=>t("div",{id:"home",children:[e("h1",{className:"text-center text-3xl md:text-6xl px-2 lg:px-32 text-[#C62937] font-bold",children:"FAKTA MENGERIKAN KANKER"}),e("p",{className:"text-center text-lg md:text-xl px-2",children:"Kanker adalah penyebab kematian ketiga terbanyak di indonesia"})]});export{x as default};
