c6e0cb032454b310bc3e8e568cfcb3550540cc00
Golfantara <golfantara7@gmail.com>
2023-04-27 10:07:16 +0700

    added-link-toko

--- src/modules/marketPlace/index.jsx
+++ src/modules/marketPlace/index.jsx
@@ -1,4 +1,4 @@
-import Line from "@/layouts/fifthLayout";
+import { Link } from "react-router-dom";
 
 const marketPlace = () => {
   return (
@@ -12,14 +12,22 @@ const marketPlace = () => {
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 mt-2 place-content-center gap-4">
         <div className="md:col-start-2 grid place-content-center">
-          <button className="bg-[#5DB552] text-[#FFE5E6] py-2 px-5 rounded-lg">
+          <Link
+            to="https://www.tokopedia.com/herbalsehatnabi/manggistech-untuk-kanker-leukemia-kanker-paru-kanker-payudara?extParam=whid%3D105882"
+            target="_blank"
+            className="bg-[#5DB552] text-[#FFE5E6] py-2 px-5 rounded-lg"
+          >
             Tokopedia
-          </button>
+          </Link>
         </div>
         <div className="md:col-start-3 grid place-content-center">
-          <button className="bg-[#E9552D] text-[#FFE5E6] py-2 px-5 rounded-lg">
+          <Link
+            to="https://shopee.co.id/Manggistech-untuk-Leukeumia-Kanker-Usus-Besar-Kanker-Paru-Kanker-Payudara-Kanker-Prostat-Kanker-Pankreas-Kanker-Kulit-Kanker-Mulut-Kanker-Tulang-Kanker-otak-Kanker-liver-Hepar-i.25592855.18914542625?sp_atk=a8f7c15a-05d9-4eee-b22c-ca5ab6be142b&xptdk=a8f7c15a-05d9-4eee-b22c-ca5ab6be142b"
+            className="bg-[#E9552D] text-[#FFE5E6] py-2 px-5 rounded-lg"
+            target="_blank"
+          >
             Shopee
-          </button>
+          </Link>
         </div>
       </div>
     </div>
